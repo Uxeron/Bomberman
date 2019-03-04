b2aa1b332764389d276e877aa9692fe233afd0cc
Uxeron <vgabrielius123@gmail.com>
2019-03-05 00:12:51 +0200

    Added basic keyboard input, character movement and screen borders

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
 const int SCREEN_WIDTH 	= 640;
 const int SCREEN_HEIGHT = 480;
 
+const int CHAR_WIDTH 	= 23;
+const int CHAR_HEIGHT 	= 56;
+
 SDL_Window *Window = NULL;
 
 // The surface contained by the window
@@ -20,14 +23,13 @@ SDL_Surface *ScreenSurface = NULL;
 // Character's image
 SDL_Surface *Character = NULL;
 
-SDL_Surface *loadSurface(std::string path)
-{
+SDL_Surface *loadSurface(std::string path) {
 	// The final optimized image
 	SDL_Surface *optimizedSurface = NULL;
 
 	// Load image at specified path
 	SDL_Surface *loadedSurface = IMG_Load(path.c_str());
-	
+
 	// Convert surface to screen format
 	optimizedSurface = SDL_ConvertSurface(loadedSurface, ScreenSurface->format, 0);
 
@@ -41,14 +43,12 @@ SDL_Surface *loadSurface(std::string path)
 }
 
 
-void draw_image(SDL_Surface *image, int x, int y)
-{
+void draw_image(SDL_Surface *image, int x, int y) {
 	SDL_Rect rect = {x, y, image->w, image->h};	// Create temporary rect
 	SDL_BlitSurface(image, NULL, ScreenSurface, &rect);	// Blit image at position
 }
 
-
-void quit() {
+void stop_game() {
 	// Free character's image
 	SDL_FreeSurface(Character);
 	Character = NULL;
@@ -70,20 +70,68 @@ int main(int argc, char *args[]) {
 	Window = SDL_CreateWindow("Bomberman!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	ScreenSurface = SDL_GetWindowSurface(Window);
 
-	// Set background to white
-	SDL_FillRect(ScreenSurface, NULL, SDL_MapRGB(ScreenSurface->format, 255, 255, 255));
-
 	// Load character image
 	Character = loadSurface("Character_Stand_Down.png");
-	
-	draw_image(Character, 100, 10);
 
-	// Update the surface
-	SDL_UpdateWindowSurface(Window);
+	// Character position
+	int x = 0;
+	int y = 0;
 
-	SDL_Delay(2000);	// Wait 2 seconds
+	// Main loop flag
+	bool quit = false;
 
-	quit();				// Stop the game
+	// Event handler
+	SDL_Event e;
+
+	// While application is running
+	while (!quit) {
+		// Handle events on queue
+		while (SDL_PollEvent(&e) != 0) {
+			// User pressed x
+			if (e.type == SDL_QUIT)
+				quit = true;
+
+			// User presses a key
+			else if (e.type == SDL_KEYDOWN) {
+				switch (e.key.keysym.sym) {
+					case SDLK_UP:
+						y -= 5;
+						if (y < 0) 
+							y = 0;
+						break;
+
+					case SDLK_DOWN:
+						y += 5;
+						if (y + CHAR_HEIGHT > SCREEN_HEIGHT)
+							y = SCREEN_HEIGHT - CHAR_HEIGHT;
+						break;
+
+					case SDLK_LEFT:
+						x -= 5;
+						if (x < 0)
+							x = 0;
+						break;
+
+					case SDLK_RIGHT:
+						x += 5;
+						if (x + CHAR_WIDTH > SCREEN_WIDTH)
+							x = SCREEN_WIDTH - CHAR_WIDTH;
+						break;
+				}
+			}
+		}
+
+		// Clear screen
+		SDL_FillRect(ScreenSurface, NULL, SDL_MapRGB(ScreenSurface->format, 0, 127, 64));
+
+		// Draw character
+		draw_image(Character, x, y);
+
+		// Update the screen
+		SDL_UpdateWindowSurface(Window);
+	}
+
+	stop_game();	// Stop the game
 	
 	return 0; 
 }
