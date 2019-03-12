3691c0819834095e38ff6f0f1969fd29f80340c5
Uxeron <vgabrielius123@gmail.com>
2019-03-12 13:37:55 +0200

    Changed include guard to pragma once

--- character.hpp
+++ character.hpp
@@ -1,20 +1,18 @@
 // Character class
-#ifndef CHARACTER_H_
-    #define CHARACTER_H_
+#pragma once 
 
-    #include "interactiveObject.hpp"
+#include "interactiveObject.hpp"
 
-    #ifdef __linux__ // linux
-        #include <SDL2/SDL.h>
-    #else // windows
-        #include "SDL2/include/SDL.h"
-    #endif
-
-    class Character: public InteractiveObject {
-    public:
-        Character(Window* wind);
-        void process();
-        void event(SDL_Event ev);
-        void draw();
-    };
+#ifdef __linux__ // linux
+    #include <SDL2/SDL.h>
+#else // windows
+    #include "SDL2/include/SDL.h"
 #endif
+
+class Character: public InteractiveObject {
+public:
+    Character(Window* wind);
+    void process();
+    void event(SDL_Event ev);
+    void draw();
+};
--- window.hpp
+++ window.hpp
@@ -1,37 +1,35 @@
 // Handles the main game Window, drawing to that Window and loading images
 // Window is a singleton (wouldn't want multiple windows)
-#ifndef WINDOW_H_
-    #define WINDOW_H_
+#pragma once 
 
-    #include <string>
-    #include <iostream>
+#include <string>
+#include <iostream>
 
-    #ifdef __linux__	// linux
-        #include <SDL2/SDL.h>
-        #include <SDL2/SDL_image.h>
-    #else 				// windows
-        #include "SDL2/include/SDL.h"
-        #include "SDL2/include/SDL_image.h"
-    #endif
-
-    class Window {
-        Window();
-        static Window* instance;
-        SDL_Window*   gameWindow    = NULL;
-        SDL_Surface*  screenSurface = NULL;
-        bool created = false;
-        int width;
-        int height;
-    public:
-        static Window* getInstance();
-        friend SDL_Surface *loadSurface(std::string path, Window wind);
-        void create(int screen_width, int screen_height, std::string name);
-        void free();
-        void drawImage(SDL_Surface *image, int x, int y);
-        void drawImage(SDL_Surface *image, SDL_Rect *rect);
-        void fillScreen(int r, int g, int b);
-        void update();
-        int getWidth();
-        int getHeight();
-    };
+#ifdef __linux__	// linux
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_image.h>
+#else 				// windows
+    #include "SDL2/include/SDL.h"
+    #include "SDL2/include/SDL_image.h"
 #endif
+
+class Window {
+    Window();
+    static Window* instance;
+    SDL_Window*   gameWindow    = NULL;
+    SDL_Surface*  screenSurface = NULL;
+    bool created = false;
+    int width;
+    int height;
+public:
+    static Window* getInstance();
+    friend SDL_Surface *loadSurface(std::string path, Window wind);
+    void create(int screen_width, int screen_height, std::string name);
+    void free();
+    void drawImage(SDL_Surface *image, int x, int y);
+    void drawImage(SDL_Surface *image, SDL_Rect *rect);
+    void fillScreen(int r, int g, int b);
+    void update();
+    int getWidth();
+    int getHeight();
+};
